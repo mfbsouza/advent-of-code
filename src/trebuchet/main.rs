805dfdfbe94fbcef748036a9f2dc3196d87149da
Matheus Souza <mfbsouza.it@gmail.com>
2026-06-23 10:11:20 -0300

    generic and trait bounds to increase code reusability

--- src/trebuchet/main.rs
+++ src/trebuchet/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::fs;
-use std::io::{self, BufRead};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 use std::process;
 
 fn main() -> io::Result<()> {
@@ -10,8 +10,8 @@ fn main() -> io::Result<()> {
         process::exit(1);
     });
 
-    let file = fs::File::open(path)?;
-    let reader = io::BufReader::new(file);
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
 
     let mut numbers: Vec<u32> = Vec::new();
     for ln in reader.lines() {
@@ -24,30 +24,25 @@ fn main() -> io::Result<()> {
 }
 
 fn parse_line(line: &str) -> Option<u32> {
-    let mut number = parse_first_digit(line).unwrap() * 10;
-    number += reverse_parse_first_digit(line).unwrap();
+    let mut number = parse_first_digit(line.chars(), &line, false).unwrap() * 10;
+    number += parse_first_digit(line.chars().rev(), &line, true).unwrap();
     Some(number)
 }
 
-fn parse_first_digit(string: &str) -> Option<u32> {
-    for (i, char) in string.chars().enumerate() {
+fn parse_first_digit<I>(iter: I, string: &str, backwards: bool) -> Option<u32>
+where
+    I: Iterator<Item = char>,
+{
+    for (index, char) in iter.enumerate() {
         if char.is_ascii_digit() {
-            return char.to_digit(10)
+            return char.to_digit(10);
         }
-        match parse_spelled_out_digit(&string[i..string.len()]) {
-            Some(n) => return Some(n),
-            None => continue,
-        }
-    }
-    None
-}
-
-fn reverse_parse_first_digit(string: &str) -> Option<u32> {
-    for (i, char) in string.chars().rev().enumerate() {
-        if char.is_ascii_digit() {
-            return char.to_digit(10)
-        }
-        match parse_spelled_out_digit(&string[string.len()-(i+1)..string.len()]) {
+        let slice_range = if !backwards {
+            index..string.len()
+        } else {
+            string.len() - (index + 1)..string.len()
+        };
+        match parse_spelled_out_digit(&string[slice_range]) {
             Some(n) => return Some(n),
             None => continue,
         }
@@ -56,15 +51,17 @@ fn reverse_parse_first_digit(string: &str) -> Option<u32> {
 }
 
 fn parse_spelled_out_digit(string: &str) -> Option<u32> {
-    let dic = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let dic = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
     if string.len() > 1 {
-        for (i, item) in dic.iter().enumerate() {
+        for (index, item) in dic.iter().enumerate() {
             if item[0..1] == string[0..1] {
                 if item.len() > string.len() {
                     continue;
                 }
                 if item[..] == string[0..item.len()] {
-                    let number = u32::try_from(i+1).expect("error converting usize to u32");
+                    let number = u32::try_from(index + 1).expect("error converting usize to u32");
                     return Some(number);
                 }
             }
